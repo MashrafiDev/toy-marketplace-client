3afb322b3f3e548b7fe2cd22a72889d9c6e39fe2
Masrafi404 <mdmasrafi902@gmail.com>
2023-05-17 21:46:44 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-red-400">Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
+);
